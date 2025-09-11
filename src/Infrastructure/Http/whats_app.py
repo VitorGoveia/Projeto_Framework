13e1e4094b739c2903b48fedf1ba32e3df583182
Guibellemo <gbellemo@gmail.com>
2025-09-11 19:25:01 -0300

    [fix] ajustando a função

--- src/Infrastructure/Http/whats_app.py
+++ src/Infrastructure/Http/whats_app.py
@@ -1,21 +1,24 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
-import os
+import os 
 from twilio.rest import Client
 import json
-#import UserDomain from src.Domain.User
+import UserDomain from src.Domain.User
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = "" #SID do Twilio tem que pegar novamente
-auth_token = "" #token do Twilio tem que pegar novamente (git ta barrando)
-client = Client(account_sid, auth_token)
 
-def send_whatsapp_code(code,phone):
+def send_whatsapp_code(code:str,phone:str):
+
+    account_sid = "" #SID do Twilio tem que pegar novamente
+    auth_token = "" #token do Twilio tem que pegar novamente (git ta barrando)
+    client = Client(account_sid, auth_token)
         
     message = client.messages.create(
     from_='whatsapp:+14155238886',
     content_sid='', #tem que pegar de novo
-    content_variables='{"1":"{code}"}',
+    content_variables=f'{{"1":"{code}"}}',
     to=f"whatsapp:+{phone}"
     )
     print(message.sid)
+
+
+#send_whatsapp_code(123456,5511952912079)
