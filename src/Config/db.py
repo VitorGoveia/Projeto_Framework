345256f65c4d4c820489a12eae23795b78cf102e
João Vitor de Matos Goveia <Vitorgoveia9381@gmail.com>
2025-11-10 15:08:55 -0300

    fix - ajustando DB.py para o Deploy

--- src/Config/db.py
+++ src/Config/db.py
@@ -1,11 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
-import psycopg2
+import os
 
 db = SQLAlchemy()
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://usuario:senha@db:5432/mamutedb'
+    database_url = os.getenv("DATABASE_URL")
+
+    if database_url and database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    if not database_url:
+        database_url = "postgresql://postgres:1234@db:5432/postgres"
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
 
     with app.app_context():
